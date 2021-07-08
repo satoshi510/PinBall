992d20e97b99baaec7b859d51cdd9aafa61a85d8
satoshi <sato-naka@kfy.biglobe.ne.jp>
2021-07-08 11:49:28 +0900

    Lesson5 課題追加
    フリッパーを動かすボタンを増やした

--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -27,23 +27,33 @@ public class FripperController : MonoBehaviour
     void Update()
     {
         //左矢印キーを押した時左フリッパーを動かす
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S)) && tag == "LeftFripperTag")
         {
             SetAngle(this.flickAngle);
         }
         //右矢印キーを押した時右フリッパーを動かす
-        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+        if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S)) && tag == "RightFripperTag")
         {
             SetAngle(this.flickAngle);
         }
         //矢印キー話された時フリッパーを元に戻す
-        if(Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        if((Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S)) && tag == "LeftFripperTag")
         {
-            SetAngle(this.defaultAngle);
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) == true)
+            { }
+            else
+            {
+                SetAngle(this.defaultAngle);
+            }
         }
-        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+        if ((Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.S)) && tag == "RightFripperTag")
         {
-            SetAngle(this.defaultAngle);
+            if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S)) == true)
+                { }
+            else
+            {
+                SetAngle(this.defaultAngle);
+            }
         }
 
     }
